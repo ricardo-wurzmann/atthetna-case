8ae61b6001b221c85c05bd09be50b5dc4b0a13b5
ricardo-wurzmann <103436943+ricardo-wurzmann@users.noreply.github.com>
2026-03-23 12:10:34 -0300

    Change default DB_PORT from 5432 to 5433

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 def get_connection():
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        port=os.getenv("DB_PORT", "5432"),
+        port=os.getenv("DB_PORT", "5433"),
         dbname=os.getenv("DB_NAME", "atthena"),
         user=os.getenv("DB_USER", "postgres"),
         password=os.getenv("DB_PASSWORD", ""),
